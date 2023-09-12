6985e2a7107c8614e13ae3efb8090ee6c7c62fdc
KKudas <143763586+KKudas@users.noreply.github.com>
2023-09-12 09:22:39 +0800

    Update script.js

--- 2102/1/script.js
+++ 2102/1/script.js
@@ -1,36 +1,32 @@
 var modal = document.getElementById('simpleModal');
 var modalBtn = document.getElementById('modalBtn');
+var updateModal = document.getElementById('simpleUpdateModal');
 
-var updateBtn = document.getElementById('updateBtn');
 var simpleUpdateModal = document.getElementById('simpleUpdateModal');
-var closeBtn = document.getElementById('sub');
 
-updateBtn.addEventListener('click', openUpModal);
 modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
 
 function openUpModal(){
-  const contactID = this.getAttribute('updateID');
-  document.querySelector('#simpleUpdateModal form').action = `methodPHP/update.php?id=${contactID}`;
   simpleUpdateModal.style.display = 'block';
-}
-
-function closeUpModal(){
-  simpleUpdateModal.style.display = 'none';
 }
 
 function openModal(){
   modal.style.display = 'block';
 }
 
-function closeModal() {
-  modal.style.display = "none";
-}
-
 window.addEventListener("click", function (event) {
-  if (event.target === modal) {
-      closeModal();
-  } else if (event.target === simpleUpdateModal) {
-    closeUpModal();
+  if (event.target === modal || event.target === updateModal) {
+    modal.style.display = "none";
+    updateModal.style.display = "none";
   }
 });
+
+function updateID(samp){
+  updateModal.style.display = "block";
+  document.cookie = `updateID?=${samp}`;
+  // window.location.href = `update.php?id=${samp}`;
+}
+
+function deleteID(samp){
+  alert(samp);
+}
